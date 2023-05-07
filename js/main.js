87bb47605f089463c29e29dde43de8c23d5e9f3d
Davlatbek <exemple@gmail.com>
2023-05-07 17:21:03 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,89 @@
+// // let UserName = "John"
+// // let number = 25;
+// // number = 24;
+// // console.log(number);
+
+
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };console.log(obj.isMarried);
+
+
+// const arr = [1, 'davlatbek', 84, 21.0,];
+// console.log(arr[1]);  
+// const arr = [ 'a', 'b', 'c'];
+// const arrObj = {
+//     0: 'a',
+//     1: 'b',
+//     2: 'c'
+// }
+
+
+// const storeName = 'Apple'
+// const storeDescription = {
+//     budget:10000,
+//     employees:['Alex', 'John', 'Sam'],
+//     products:{
+//         'Iphone':20,
+//         'Macbook':5
+//     },
+//     open:true
+// }
+
+
+// const result = confirm("Are you here ?");
+// console.log(result);
+
+
+// const answer = prompt('Как ваша имя?', '' );
+// console.log(answer);
+
+
+// const answers = prompt('Как ваша фамилия?', '' );
+// console.log(typeof(answers));
+
+
+// const answerss = [];
+// answerss[0] = prompt('Где вы работаете?');
+// answerss[1] = prompt('Где вы хотели работать?');
+// answerss[2] = prompt('Какое заробатную плату вы хотели?');
+// document.write(answerss);
+// onsole.log(answerss);
+
+
+// const category = 'toys';
+// const cars = 'BMW';
+// console.log(`https://someurl.com/${category}/5/${cars}/2`);
+
+
+// const user = 'Ivan';
+// // alert(`Hello , ${user}`);
+
+
+// const Name  = 'messi';
+// const Surename = 'leo';
+// const fullName = 'Leo Messi';
+// console.log(`https://barselona.com/${Name}/1/${Surename}/10/${fullName}/30`);
+
+
+// let incr = 10,
+//     decr = 10;
+// incr++;
+// decr--;
+// console.log(incr);
+// console.log(decr);
+
+
+// let result = 5%2;
+// console.log(result);
+// console.log(4*2 === '8');
+
+
+// const a = true,
+//       b = true;
+// console.log(a && b);
+
+
+//  
